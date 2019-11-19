073af4d66e6e01a2ba9ffdfeedfebf5831b46fd8
Nikita Ivanov <www.i.ru94@yandex.ru>
2019-11-19 11:36:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 4001;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 });
